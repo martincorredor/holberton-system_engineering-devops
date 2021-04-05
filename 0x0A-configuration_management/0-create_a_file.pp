887fd3943bb270bcce982ecfb35413c1eb2db3b2
martincorredor <dev.martincorredor@gmail.com>
2021-04-05 14:50:26 -0500

    Code to task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
-# create a file in /tmp.
-
-file { '/tmp/holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+# create a file in /tmp
+file {'/tmp/holberton': # name of the file
+    ensure  => 'file', # ensure the creation of the file
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    mode    => '0744',
 }
